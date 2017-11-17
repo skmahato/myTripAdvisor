def392ad4a22b81f8e5873baf1fd015fd0897a9e
skmahato <souravkumar1329@gmail.com>
2017-11-17 14:27:24 +0530

    Added search field for hotel check in and check out.... User will give the check_in and check_out date with other parameters like hotel name, location, min-price, max-price and the search  will provied the available hotels...

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,39 +1,46 @@
 class Search < ApplicationRecord
-  def products
-    @products ||= find_products
+  def hotels
+    @hotels ||= find_hotels
 
   end
 
   private
 
-    def find_products
-      products = Hotel.all
-      products=products.where('hotel_name LIKE ?', "%#{name}%") if name.present?
-      products=products.where("location LIKE ?", "%#{location}%") if location.present?
-      products=products.where("price >= ?", min_price) if min_price.present?
-      products=products.where("price <= ?",max_price) if max_price.present?
-      products
+    def find_hotels
+      hotels = Hotel.all
+      hotels=hotels.where('hotel_name LIKE ?', "%#{name}%") if name.present?
+      hotels=hotels.where("location LIKE ?", "%#{location}%") if location.present?
+      hotels=hotels.where("price >= ?", min_price) if min_price.present?
+      hotels=hotels.where("price <= ?",max_price) if max_price.present?
+      hotels
 
       if check_in.present?
         if check_out.present?
           h=Array.new
           stay = (check_out - check_in).to_i
-          products.each do |hotel|
+          hotels.each do |hotel|
             books=hotel.bookings.order(check_in: :asc)
             d=DateTime.now
             d=d.to_i
               books.each do |book|
 
-                if ((book.check_in.to_i-d) >= stay)
-                  h<<book.hotel
-                  break
+                if (book.check_in.to_i<=check_in.to_i)
+                  d=book.check_out.to_i
+
+                else
+                  if ((book.check_in.to_i-d)>=stay)
+                    if check_in.to_i>d
+                        h<<book.hotel
+                    end
+
+                  end
                 end
 
-                d=book.check_out.to_i
+
               end
           end
         end
-      h
+      h=h.uniq { |x| x.id }
       end
     end
 end
