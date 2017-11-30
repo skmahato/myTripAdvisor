35e03ce225d2c23ff7c4a76cec305eb524e08d69
skmahato <souravkumar1329@gmail.com>
2017-11-30 15:55:51 +0530

    Added the twitter key and secret

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV["TWITTER_KEY"], ENV["TWITTER_SECRET"]
+  # provider :twitter, ENV["TWITTER_KEY"], ENV["TWITTER_SECRET"]
+  provider :twitter, "LZ2Vo7LxlKZqg4ITxLLVmw2Al", "26mDD4ZhpEB1dXyVuJK62vNMgsWg5YaKkBjFkUG45q9HEneUTD"
 end
